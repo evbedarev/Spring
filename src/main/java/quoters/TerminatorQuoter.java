e124e163f02db9cf65e2b3f137c141f878f9df2b
madjo85@yandex.ru <madjo85@yandex.ru>
2018-10-05 12:36:39 +0300

    first commit

---
+++ src/main/java/quoters/TerminatorQuoter.java
@@ -0,0 +1,32 @@
+package quoters;
+
+import javax.annotation.PostConstruct;
+
+public class TerminatorQuoter implements Quoter {
+    @InjectRandomInt(min  = 2, max = 7)
+    private int repeat;
+
+    @PostConstruct
+    public void init() {
+        System.out.println("Phase 2");
+        System.out.println(repeat);
+    }
+
+    public TerminatorQuoter() {
+        System.out.println("Phase 1");
+        System.out.println(repeat);
+    }
+
+    private String message;
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public void sayQuote() {
+        for (int i = 0; i < repeat; i++) {
+            System.out.println(message);
+        }
+    }
+}
