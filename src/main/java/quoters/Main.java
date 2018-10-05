73750e8a5f8df6a5aae65b453293c54a513636ac
madjo85@yandex.ru <madjo85@yandex.ru>
2018-10-05 17:06:41 +0300

    first commit sec

--- src/main/java/quoters/Main.java
+++ src/main/java/quoters/Main.java
@@ -3,8 +3,11 @@ package quoters;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
-        context.getBean(TerminatorQuoter.class).sayQuote();
+        while (true) {
+            Thread.sleep(1000);
+            context.getBean(Quoter.class).sayQuote();
+        }
     }
 }
